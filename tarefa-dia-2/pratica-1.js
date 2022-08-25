ed86a867e58ff3caad0d2ec89c1e3e1133ca40cf
Fabio Freitas <fabiootavio04@gmail.com>
2022-08-25 14:52:47 -0300

    meu quinto codigo

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -3,15 +3,19 @@
 
 
 let nome = "Fabio Freitas"
-
+console.log(nome)
 
 let email = "fabiootavio04@gmail.com"
-
+console.log(email)
 
 let profissao = "autonomo"
-
+console.log(profissao)
 
 let idade = 21
-
+console.log(idade)
 
 let conhecer = "Orlando"
+console.log(conhecer)
+
+let mensagem = `Olá, ${nome}! Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para ${conhecer}. Confirma pra gente alguns dados? A sua idade é ${idade}, você é ${profissao} e o seu email é ${email}? Ficamos no aguardo. Muito obrigado e boa viagem! :D`
+console.log(mensagem) 
